8df15c3d086a34fb97c45e9e104af1ce4b545a2f
David Caffrey <dave.caffrey71@gmail.com>
2023-04-28 06:50:48 +0100

    finished sticky, added browser compatibilty

--- js/script.js
+++ js/script.js
@@ -46,14 +46,24 @@ allLinks.forEach(function (link) {
 const sectionHeroEl = document.querySelector('.section-hero');
 
 
-const observer = new IntersectionObserver(function(entries) {
-  const ent = entries[0];
-  console.log(ent);
-},{
-  // In the viewport
-  root: null,
-  threshold: 0,
-})
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
 observer.observe(sectionHeroEl);
 
 
